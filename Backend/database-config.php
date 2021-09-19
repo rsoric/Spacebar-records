03f3ce230a99c90905dd6aeaa94eabf0c021225f
rsoric <robert.soric@gmail.com>
2021-09-20 00:07:46 +0200

    Updated db config

--- Backend/database-config.php
+++ Backend/database-config.php
@@ -2,9 +2,9 @@
 
     final class DBConfig
     {
-        const serverName = "localhost";
-        const dbUsername = "root";
-        const dbPassword = "";
+        const serverName = "eu-cdbr-west-01.cleardb.com";
+        const dbUsername = "b2c4afb48a6351";
+        const dbPassword = "d03f04dc";
         const dbName = "homebase";
     }
 
