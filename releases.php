4a5418aad90e7ad4694dac47cbf61a2800699b05
rsoric <robert.soric@gmail.com>
2021-09-20 22:26:17 +0200

    Update releases.php

--- releases.php
+++ releases.php
@@ -1,8 +1,7 @@
 <?php 
 include_once "header.php";
 
-define("FS_ROOT", realpath(dirname(__FILE__)));
-include_once FS_ROOT.'\Backend\releases.php'; ?>
+include_once '\Backend\releases.php'; ?>
 
 <main class="px-3">
 
