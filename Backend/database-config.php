eb26b51952ba5dd73871f97ae191a35aff5da8fc
rsoric <robert.soric@gmail.com>
2021-09-20 10:36:14 +0200

    Db config 3

--- Backend/database-config.php
+++ Backend/database-config.php
@@ -5,6 +5,6 @@
         const serverName = "eu-cdbr-west-01.cleardb.com";
         const dbUsername = "bcf487fdd3ecd2";
         const dbPassword = "e5923fc5";
-        const dbName = "homebase";
+        const dbName = "heroku_ba0aa51a5c78d32";
     }
 
